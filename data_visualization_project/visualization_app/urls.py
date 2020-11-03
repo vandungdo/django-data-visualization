99d2e3cbf7c9b61fe811d6199e0a0869077250f8
dung <monsieurdung@gmail.com>
2020-11-04 00:16:28 +0100

    add relative urls

--- data_visualization_project/visualization_app/urls.py
+++ data_visualization_project/visualization_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from visualization_app import views
 
+
+app_name = 'visualization_app'
+
 urlpatterns = [
-    path('', views.index),
-    path('interactive', views.interactive_plot),
-    path('static', views.static_plot)
+    path('interactive', views.interactive_plot, name="interactive"),
+    path('static', views.static_plot, name="static")
 
 ]
--- data_visualization_project/visualization_app/views.py
+++ data_visualization_project/visualization_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello')
+    return render(request, 'index.html')
 
 def interactive_plot(request):
 
